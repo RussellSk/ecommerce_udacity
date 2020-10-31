2bd373f62feac623dec8c4fb52c80f7638c948e0
Russell Skalden <russell.skalden@gmail.com>
2020-10-31 21:31:30 +0500

    UserControllerTest

--- src/test/java/com/example/demo/controllers/UserControllerTest.java
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -1,29 +1,24 @@
 package com.example.demo.controllers;
 
 import com.example.demo.SareetaApplication;
-import com.example.demo.model.persistence.Cart;
-import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.User;
-import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
-import java.math.BigDecimal;
 
 @Transactional
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SareetaApplication.class)
 public class UserControllerTest {
-    private final BCryptPasswordEncoder bCryptPasswordEncoder = Mockito.mock(BCryptPasswordEncoder.class);
-
-    @Autowired
-    private UserRepository userRepository;
-
     @Autowired
     private UserController userController;
 
@@ -33,9 +28,50 @@ public class UserControllerTest {
         createUserRequest.setUsername("user1");
         createUserRequest.setPassword("testPassword");
         createUserRequest.setConfirmPassword("testPassword");
-
-
+        ResponseEntity<User> responseEntity = userController.createUser(createUserRequest);
+        Assert.assertNotNull(responseEntity);
+        Assert.assertEquals(200, responseEntity.getStatusCodeValue());
+        User createdUser = responseEntity.getBody();
+        Assert.assertNotNull(createdUser);
+        Assert.assertEquals(createUserRequest.getUsername(), createdUser.getUsername());
+        Assert.assertTrue(createdUser.getId() > 0);
     }
 
+    @Test
+    public void findById() {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("userTest");
+        createUserRequest.setPassword("testPassword");
+        createUserRequest.setConfirmPassword("testPassword");
+        ResponseEntity<User> createUserResponse = userController.createUser(createUserRequest);
+        Assert.assertEquals(200, createUserResponse.getStatusCodeValue());
+        Assert.assertNotNull(createUserResponse);
+        User createdUserModel = createUserResponse.getBody();
 
+        ResponseEntity<User> responseEntity = userController.findById(1L);
+        Assert.assertEquals(200, responseEntity.getStatusCodeValue());
+        Assert.assertNotNull(responseEntity);
+        User foundUser = responseEntity.getBody();
+        Assert.assertNotNull(foundUser);
+        Assert.assertEquals(1L, foundUser.getId());
+    }
+
+    @Test
+    public void findByUserName() {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("userTest");
+        createUserRequest.setPassword("testPassword");
+        createUserRequest.setConfirmPassword("testPassword");
+        ResponseEntity<User> createUserResponse = userController.createUser(createUserRequest);
+        Assert.assertEquals(200, createUserResponse.getStatusCodeValue());
+        Assert.assertNotNull(createUserResponse);
+        User createdUserModel = createUserResponse.getBody();
+
+        ResponseEntity<User> responseEntity = userController.findByUserName(createUserRequest.getUsername());
+        Assert.assertEquals(200, responseEntity.getStatusCodeValue());
+        Assert.assertNotNull(responseEntity);
+        User foundUser = responseEntity.getBody();
+        Assert.assertNotNull(foundUser);
+        Assert.assertEquals(createUserRequest.getUsername(), foundUser.getUsername());
+    }
 }
